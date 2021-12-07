1b01758d76b4cd1197c23dbf354e1f97511bdc7a
ROBERT P CAREY III <rpc5102@psu.edu>
2021-12-07 10:41:57 -0500

    Fixed: Quickedit form

--- js/form-actions.js
+++ js/form-actions.js
@@ -1,20 +1,24 @@
 Drupal.behaviors.selectAllFormItems = {
   attach: function (context, settings) {
     // Attach a click listener to the select all button
-    document.getElementById('edit-settings-books-displayed-select-all-books').addEventListener('click', function(evt) {
+    document.querySelectorAll('[id^=edit-settings-books-displayed-select-all-books]').forEach((btn) => {
+      btn.addEventListener('click', function(evt) {
+        console.log('oi');
         evt.preventDefault();
-        var books = document.getElementById('edit-settings-books-displayed-selection--wrapper').querySelectorAll('input[type="checkbox"]');
-        for (const book in books) {
-          books[book].checked = true;
-        }
-    }, false);
+        btn.parentElement.querySelectorAll('[id^=edit-settings-books-displayed-selection] input[type="checkbox"]').forEach(book => {
+          book.checked = true;
+        });
+      }, false)
+    });
+
     // Attach a click listener to the deselect all button
-    document.getElementById('edit-settings-books-displayed-deselect-all-books').addEventListener('click', function(evt) {
-      evt.preventDefault();
-      var books = document.getElementById('edit-settings-books-displayed-selection--wrapper').querySelectorAll('input[type="checkbox"]');
-      for (const book in books) {
-        books[book].checked = false;
-      }
-    }, false);
+    document.querySelectorAll('[id^=edit-settings-books-displayed-deselect-all-books]').forEach((btn) => {
+      btn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        btn.parentElement.querySelectorAll('[id^=edit-settings-books-displayed-selection] input[type="checkbox"]').forEach(book => {
+          book.checked = false;
+        });
+      }, false)
+    });
   }
 };
